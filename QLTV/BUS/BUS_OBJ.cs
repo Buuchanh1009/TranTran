b49d7c251a353674b71e90418178b9009b4651bf
Bao Tran <tachanh10999@gmail.com>
2019-05-29 12:29:15 +0700

    first commit

---
+++ QLTV/BUS/BUS_OBJ.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAO;
+using DTO;
+
+namespace BUS
+{
+    public class BUS_OBJ
+    {
+        public static object layLoaiTaiKhoan(string tenDN, string mk)
+        {
+            return Sach_DAO.loaiTaiKhoan(tenDN, mk);
+        }
+        public static string layTenSach(string maSach)
+        {
+            return Sach_DAO.layTenSach(maSach);
+        }
+        public static string layTenDocGia(string maDocGia)
+        {
+            return Sach_DAO.layTenDocGia(maDocGia);
+        }
+        public static string layTenTacGia(string maTacGia)
+        {
+            return Sach_DAO.layTenTacGia(maTacGia);
+        }
+        public static string layTenTheLoai(string maTheLoai)
+        {
+            return Sach_DAO.layTenTheLoai(maTheLoai);
+        }
+        public static string layTenNXB(string maNXB)
+        {
+            return Sach_DAO.layTenNXB(maNXB);
+        }
+        public static string layMaTheTheoMaDocGia(string maDocGia)
+        {
+            return Sach_DAO.layMaTheTheoMaDocGia(maDocGia);
+        }
+        
+        public static int demSoPhieuMuonChoDuyet()
+        {
+            return Sach_DAO.demSoPhieuMuonChoDuyet();
+        }
+        public static int demSoPhieuMuonTreHan()
+        {
+            return Sach_DAO.demSoPhieuMuonTreHan();
+        }
+        public static void taoMoiTKNV(string tenDN, string mk)
+        {
+            Sach_DAO.taoMoiTK(tenDN,mk);
+        }
+        public static void doiMK(string taiKhoan, string mkMoi)
+        {
+            Sach_DAO.doiMK(taiKhoan, mkMoi);
+        }
+        public static DataTable loadDSSach()
+        {
+            return Sach_DAO.loadDSSach();
+        }
+        public static DataTable loadDSTacGia()
+        {
+            return Sach_DAO.loadDSTacGia();
+        }
+        public static DataTable loadDSTheLoai()
+        {
+            return Sach_DAO.loadDSTheLoai();
+        }
+        public static DataTable loadDSNXB()
+        {
+            return Sach_DAO.loadDSNXB();
+        }
+        public static DataTable loadDSDocGia()
+        {
+            return Sach_DAO.loadDSDocGia();
+        }
+        public static DataTable loadDSLoaiDG()
+        {
+            return Sach_DAO.loadDSLoaiDG();
+        }
+        public static DataTable loadDSLoaiThe()
+        {
+            return Sach_DAO.loadDSLoaiThe();
+        }
+        public static DataTable loadDSThe()
+        {
+            return Sach_DAO.loadDSThe();
+        }
+        public static void themSach(Sach_DTO sach)
+        {
+            Sach_DAO.themSach(sach);
+        }
+        public static void suaSach(Sach_DTO sach)
+        {
+            Sach_DAO.suaSach(sach);
+        }
+        public static void xoaSach(string maSach)
+        {
+            Sach_DAO.xoaSach(maSach);
+        }
+        public static void themTG(TacGia_DTO tacGia)
+        {
+            Sach_DAO.themTG(tacGia);
+        }
+        public static void suaTG(TacGia_DTO tacGia)
+        {
+            Sach_DAO.suaTG(tacGia);
+        }
+        public static void xoaTG(string maTacGia)
+        {
+            Sach_DAO.xoaTG(maTacGia);
+        }
+        public static void themNXB(NXB_DTO nXB)
+        {
+            Sach_DAO.themNXB(nXB);
+        }
+        public static void suaNXB(NXB_DTO nXB)
+        {
+            Sach_DAO.suaNXB(nXB);
+        }
+        public static void xoaNXB(string maNXB)
+        {
+            Sach_DAO.xoaNXB(maNXB);
+        }
+        public static void themTL(TheLoai_DTO theLoai)
+        {
+            Sach_DAO.themTL(theLoai);
+        }
+        public static void suaTL(TheLoai_DTO theLoai)
+        {
+            Sach_DAO.suaTL(theLoai);
+        }
+        public static void xoaTL(string maTheLoai)
+        {
+            Sach_DAO.xoaTL(maTheLoai);
+        }
+       
+        public static void themThe(The_DTO the)
+        {
+            Sach_DAO.themThe(the);
+        }
+        public static void themUser(string tenDN, string mK, int type)
+        {
+            Sach_DAO.themUser(tenDN, mK, type);
+        }
+        public static void suaUser(string tenDN, string mK, int type)
+        {
+            Sach_DAO.suaUser(tenDN, mK, type);
+        }
+        public static void xoaUser(string maDG)
+        {
+            Sach_DAO.xoaUser(maDG);
+        }
+        public static void themDG(DocGia_DTO docGia)
+        {
+            Sach_DAO.themDG(docGia);
+        }
+        public static void suaDG(DocGia_DTO docGia)
+        {
+            Sach_DAO.suaDG(docGia);
+        }
+        public static void xoaDG(string maDocGia)
+        {
+            Sach_DAO.xoaDG(maDocGia);
+        }
+        public static void xoaThe(string maThe)
+        {
+            Sach_DAO.xoaThe(maThe);
+        }
+        public static void suaThe(The_DTO the)
+        {
+            Sach_DAO.suaThe(the);
+        }
+        
+       
+        public static string layMaDocGiaTheoTenDocGia(string tenDocGia)
+        {
+            return Sach_DAO.layMaDocGiaTheoTenDocGia(tenDocGia);
+        }
+        public static string layMaSachTheoTenSach(string tenSach)
+        {
+            return Sach_DAO.layMaSachTheoTenSach(tenSach);
+        }
+       
+        public static DocGia_DTO layTTDocGia(string maDocGia)
+        {
+            return DocGia_DAO.layTTDocGia(maDocGia);
+        }
+
+        public static string layTenLoaiDocGia(string maDocGia)
+        {
+            return DocGia_DAO.layTenLoaiDocGia(maDocGia);
+        }
+
+        public static The_DTO layTTThe(string maThe)
+        {
+            return The_DAO.layTTThe(maThe);
+        }
+
+    }
+}
---
+++ QLTV/QLTV 3 tier/frmMain.cs
@@ -0,0 +1,246 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DTO;
+using BUS;
+
+namespace QLTV
+{
+    public partial class frmMain : MetroForm
+    {
+
+        private DTO.DangNhap_DTO user;
+        public delegate void delPassUserData(DangNhap_DTO user);
+        private Boolean isRunning;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            menuStripMain.Enabled = false;
+            user = new DangNhap_DTO();
+            txtUser.Focus();
+            
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.FocusMe();
+        }
+
+        public void showInfo()
+        {
+            if (this.user.TenDangNhap != "") {
+                if (this.user.LoaiTaiKhoan == 1)
+                {
+                    
+                    groupboxLogin.Hide();
+                    lbXinChao.Text = "Xin chào " + user.TenDangNhap  + " !";
+                }
+                else
+                {
+                    groupboxLogin.Hide();                    
+                    
+                    lbXinChao.Text = "Xin chào " + BUS_OBJ.layTenDocGia(user.TenDangNhap) +" !";
+                    
+                }
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MetroFramework.MetroMessageBox.Show(this,"Bạn có muốn thoát khỏi chương trình không ?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes) {
+                isRunning = false;
+                e.Cancel = false;
+            }
+            else
+                e.Cancel = true;
+        }
+       
+
+        private void hieuChinhDGV()
+        {
+            datagvSach.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            datagvSach.Columns[0].HeaderText = "Mã sách";
+            datagvSach.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            datagvSach.Columns[1].HeaderText = "Tên sách";
+            datagvSach.Columns[1].Width = 180;
+            datagvSach.Columns[2].HeaderText = "Mã tác giả";
+
+            datagvSach.Columns[3].HeaderText = "Mã thể loại";
+
+            datagvSach.Columns[4].HeaderText = "Nhà XB";
+
+            datagvSach.Columns[5].HeaderText = "Năm XB";
+
+            datagvSach.Columns[6].HeaderText = "Số trang";
+
+            datagvSach.Columns[7].HeaderText = "Giá";
+
+            datagvSach.Columns[8].HeaderText = "Số bản";
+
+            datagvSach.Columns[9].HeaderText = "Tồn";
+
+        }      
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            menuStripMain.Enabled = false;
+            lbXinChao.Text = "Xin chào ";
+            groupboxLogin.Show();
+            txtUser.Text = "";
+            txtPwd.Text = "";
+            user.TenDangNhap = "";
+            user.MatKhau = "";
+            user.LoaiTaiKhoan = 10;
+        }
+
+        private void đổiMậtKhẩuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmDoiMK doiMK = new frmDoiMK(this);
+            delPassUserData del = new delPassUserData(doiMK.receivingData);
+            del(this.user);
+            this.Opacity = .75;
+            doiMK.ShowDialog();
+        }
+
+        private void tạoMớiTKNVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmTaoMoiTKNV taoMoiTKNV = new frmTaoMoiTKNV(this);
+            this.Opacity = .75;
+            taoMoiTKNV.ShowDialog();
+        }
+
+        private void cậpNhậtSáchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCapNhatSach capNhatSach = new frmCapNhatSach(this);
+            this.Opacity = .75;
+            capNhatSach.ShowDialog();
+        }
+
+        private void cậpNhậtTácGiảToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCapNhatTacGia capNhatTG = new frmCapNhatTacGia(this);
+            this.Opacity = .75;
+            capNhatTG.ShowDialog();
+        }
+
+        private void cậpNhậtNXBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCapNhatNXB capNhatNXB = new frmCapNhatNXB(this);
+            this.Opacity = .75;
+            capNhatNXB.ShowDialog();
+        }
+
+        private void cậpNhậtThểLoạiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCapNhatTheLoai capNhatTheLoai = new frmCapNhatTheLoai(this);
+            this.Opacity = .75;
+            capNhatTheLoai.ShowDialog();
+        }
+
+        private void cậpNhậtĐộcGiảToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCapNhatDocGia capNhatDocGia = new frmCapNhatDocGia(this);
+            this.Opacity = .75;
+            capNhatDocGia.ShowDialog();
+        }
+
+        private void cậpNhậtThẻToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCapNhatThe capNhatThe = new frmCapNhatThe(this);
+            this.Opacity = .75;
+            capNhatThe.ShowDialog();
+        }
+                                                       
+        private void tmShowInfo_Tick(object sender, EventArgs e)
+        {
+            if (!this.isRunning)
+            {
+                tmShowInfo.Enabled = false;
+            }
+            else
+            {
+                showInfo();
+            }
+        }
+        public int i = 10;
+        private void timertieude_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnLogin_Click_1(object sender, EventArgs e)
+        {
+            if (txtUser.Text != "")
+            {
+                object objLoaiTK = BUS_OBJ.layLoaiTaiKhoan(txtUser.Text, txtPwd.Text);
+                if (objLoaiTK != null)
+                {
+                    if (objLoaiTK.ToString() == "1")// quyền admin
+                    {
+                        MetroFramework.MetroMessageBox.Show(this, "Đăng nhập thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        menuStripMain.Enabled = true;
+                        tàiKhoảnToolStripMenuItem.Enabled = true;
+                        quảnLýHệThốngToolStripMenuItem.Enabled = true;
+                        tạoMớiTKNVToolStripMenuItem.Enabled = true;
+                        user.TenDangNhap = txtUser.Text;
+                        user.MatKhau = txtPwd.Text;
+                        user.LoaiTaiKhoan = Convert.ToInt16(objLoaiTK);
+
+                        this.showInfo();
+                    }
+                    else if (objLoaiTK.ToString() == "0")// quyền user
+                    {
+                        MetroFramework.MetroMessageBox.Show(this, "Đăng nhập thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        menuStripMain.Enabled = true;
+                        tàiKhoảnToolStripMenuItem.Enabled = true;
+                        quảnLýHệThốngToolStripMenuItem.Enabled = false;
+                        tạoMớiTKNVToolStripMenuItem.Enabled = false;
+                        user.TenDangNhap = txtUser.Text;
+                        user.MatKhau = txtPwd.Text;
+                        user.LoaiTaiKhoan = Convert.ToInt16(objLoaiTK);
+                        this.showInfo();
+                    }
+                }
+                else
+                {
+                    MetroFramework.MetroMessageBox.Show(this, "Sai tên đăng nhập hoặc mật khẩu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtUser.Text = "";
+                    txtPwd.Text = "";
+                    txtUser.Focus();
+                }
+            }
+            else
+            {
+                MetroFramework.MetroMessageBox.Show(this, "Sai tên đăng nhập hoặc mật khẩu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtUser.Text = "";
+                txtPwd.Text = "";
+                txtUser.Focus();
+            }
+        }
+        
+
+        private void monthCalendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void quảnLýHệThốngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
